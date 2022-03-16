9f335e198076783b8f323bf460747232fe986bf2
danbylgakov79 <93380109+danbylgakov79@users.noreply.github.com>
2022-03-16 17:46:23 +0300

    Update settings.py

--- Forum/settings.py
+++ Forum/settings.py
@@ -16,16 +16,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zy4xs$@n(#utt42j&0nrj4*4z91q6_v1kn0(%adr^i6=jdwbi7'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['192.168.0.108', '127.0.0.1']
 
 
 # Application definition
@@ -79,16 +69,7 @@ WSGI_APPLICATION = 'Forum.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'forum',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
-}
+
 
 
 # Password validation
