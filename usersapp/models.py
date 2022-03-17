dc0afa569694ee921792b4d860198ac4118a303d
danbylgakov79 <93380109+danbylgakov79@users.noreply.github.com>
2022-03-17 16:28:33 +0300

    регистрация новых юзеров

---
+++ usersapp/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class UsersModel(models.Model):
+    id = models.IntegerField(primary_key=True)
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField()
+    is_superuser = models.IntegerField()
+    user_name = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.CharField(max_length=75)
+    is_staff = models.IntegerField()
+    is_active = models.IntegerField()
+    data_joined =models.DateTimeField()
+
+
