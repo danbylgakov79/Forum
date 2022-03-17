03c5e8f532a783d750f2bdda692115f8a9327e76
danbylgakov79 <93380109+danbylgakov79@users.noreply.github.com>
2022-03-18 00:20:16 +0300

    Регистрация пользователей, автоматическая выдача прав

--- usersapp/forms.py
+++ usersapp/forms.py
@@ -7,14 +7,10 @@ from django import forms
 
 
 
-class UserRegistrationForm(forms.Form):
-    password = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
-
-
+class UserRegistrationForm(ModelForm):
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'email')
+        fields = ('username', 'first_name', 'email','password')
 
     def clean_password2(self):
         cd = self.cleaned_data
--- usersapp/views.py
+++ usersapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User, Group
 from .forms import UserRegistrationForm
 
 def register(request):
@@ -12,6 +12,8 @@ def register(request):
             new_user.set_password(user_form.cleaned_data['password'])
             # Save the User object
             new_user.save()
+            g = Group.objects.get(name="Members")
+            g.user_set.add(new_user)
             return render(request, '/', {'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
