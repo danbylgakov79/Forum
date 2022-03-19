b2516f46045c3e40d2582a7a42348e5b1ae647ca
danbylgakov79 <93380109+danbylgakov79@users.noreply.github.com>
2022-03-19 17:22:42 +0300

    Исправлена регистрация

--- usersapp/views.py
+++ usersapp/views.py
@@ -14,7 +14,7 @@ def register(request):
             new_user.save()
             g = Group.objects.get(name="Members")
             g.user_set.add(new_user)
-            return render(request, '', {'new_user': new_user})
+            return render(request, 'registration/register_done.html', {'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
-        return render(request, 'registration/register.html', {'user_form': user_form})
+    return render(request, 'registration/register.html', {'user_form': user_form})
